ff3316bc9c8a1f615c0901f05bba49f8a6e772df
tushar <tony440stark@gmail.com>
2021-06-17 02:33:27 +0530

    bank details

--- assets/js/displayPlan.js
+++ assets/js/displayPlan.js
@@ -1,13 +1,22 @@
 document.getElementById('term-plan-img').addEventListener('click', ()=>{
     console.log('Hey');
+    document.getElementById('part2').style.height="16.5rem";
+    document.getElementById('Health-Plan').style.display = 'none';
+    document.getElementById('Vehicle-Plan').style.display = 'none';
     document.getElementById('Term-Plan').style.display = 'block';
 });
 
 document.getElementById('vehicle-plan-img').addEventListener('click', ()=>{
+    document.getElementById('part2').style.height="15rem";
+    document.getElementById('Term-Plan').style.display = 'none';
+    document.getElementById('Health-Plan').style.display = 'none';
     document.getElementById('Vehicle-Plan').style.display = 'block';
 });
 
 document.getElementById('health-plan-img').addEventListener('click', ()=>{
+    document.getElementById('part2').style.height="20.5rem";
+    document.getElementById('Vehicle-Plan').style.display = 'none';
+    document.getElementById('Term-Plan').style.display = 'none';
     document.getElementById('Health-Plan').style.display = 'block';
 });
 
