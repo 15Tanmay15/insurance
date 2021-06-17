96d3d3dd35c1ede02b9d1fdfae4f7f816ee239bc
tushar <tony440stark@gmail.com>
2021-06-17 21:36:14 +0530

    bugs

--- assets/js/bankDetailsbtn.js
+++ assets/js/bankDetailsbtn.js
@@ -1,3 +1,19 @@
 // document.getElementById('bankDetailsbtn').addEventListener('click', () =>{
 //     document.getElementById('bankDetailsbtn').disabled = true
 // })
+
+var width,height;
+window.onresize = window.onload = function() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  if(width<591){
+    document.getElementsByClassName('get')[0].placeholder="Bank's name";
+    document.getElementsByClassName('get')[1].placeholder='Account Number';
+    document.getElementsByClassName('get')[2].placeholder="bank's IFSC Code";
+    }
+    else{
+    document.getElementsByClassName('get')[0].placeholder="Enter your bank's name";
+    document.getElementsByClassName('get')[1].placeholder='Enter your Account Number';
+    document.getElementsByClassName('get')[2].placeholder="Enter your bank's IFSC Code";
+  }
+}
