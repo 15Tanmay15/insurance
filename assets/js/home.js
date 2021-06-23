66e9852930181082771b094e9e1db6257115a9ed
tushar <tony440stark@gmail.com>
2021-06-24 02:00:15 +0530

    done something

--- assets/js/home.js
+++ assets/js/home.js
@@ -9,3 +9,33 @@ dropdownItem[i].addEventListener('focus', function(){
 
 // videotag=document.querySelector('video');
 // setInterval(function (){videotag.play()},1);
+// var c=0;
+// setInterval(() => {
+//     if(c==0){
+//         document.getElementById('image').style.animation='image 1s'
+//         document.getElementById('image').src="../images/meet.jpg";
+//         c+=1;
+//     }
+//     else{
+//         document.getElementById('image').style.animation='image 1s'
+//         document.getElementById('image').src="../images/hey.jpg";
+//         c-=1;
+//     }
+// }, 5000);
+
+var im = document.getElementById("image");
+
+var images = ["../images/meet.jpg","../images/hey.jpg"];
+var index=0;
+
+function changeImage()
+{
+    im.setAttribute("src", images[index]);
+    index++;
+    if(index >= images.length)
+    {
+        index=0;
+    }
+}
+
+setInterval(changeImage, 3000);
