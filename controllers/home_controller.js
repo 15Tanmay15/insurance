446bd7774cc210cbdb7b8f3df5d2adafbf8d9b2b
tushar <tony440stark@gmail.com>
2021-06-27 02:09:28 +0530

    responsive cal

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = (req, res) => {
     return res.render('home')
 }
+
+module.exports.emi_calculator = (req,res) => {
+    return res.render('emi_calculator')
+}
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const homeController = require('../controllers/home_controller')
 
 router.get('/', homeController.home);
 router.use('/connector', require('./connector.js'));
-
+router.get('/emi_calculator',homeController.emi_calculator)
 module.exports = router;
