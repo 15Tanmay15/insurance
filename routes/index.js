e06da235e7ba8c271f1ffc53809f6a2e176d00a0
Tanmay <tanmaygpro@gmail.com>
2021-06-27 21:00:25 +0530

    basic changes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller')
 
 router.get('/', homeController.home);
+router.get('/emi-calculator', homeController.emi_calculator);
 router.use('/connector', require('./connector.js'));
 
 module.exports = router;
