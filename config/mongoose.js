4672ddfdf62006dda24430689ff422dd7c3a9314
Tanmay <tanmaygpro@gmail.com>
2021-06-29 07:08:31 +0530

    error fixed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ts2insurance:Tanmay@9590@cluster0.57suw.mongodb.net/ts2insurance?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://ts2insurance:Tanmay%409590@cluster0.57suw.mongodb.net/ts2insurance?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 
