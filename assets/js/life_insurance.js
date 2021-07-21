da710267d26de6252b0aac3b23c73e4550ed323c
tushar <tony440stark@gmail.com>
2021-07-21 22:15:59 +0530

    now

--- assets/js/life_insurance.js
+++ assets/js/life_insurance.js
@@ -11,7 +11,7 @@ var var2 = document.getElementById('middle')
 var var3 = document.getElementById('life-last')
 var var4 = document.getElementById('anime')
 var var5 = document.getElementById('select')
-var var6 = document.getElementsByClassName('form-life')[0]
+var var6 = document.getElementsByClassName('body')[0]
 var var8 = document.getElementsByClassName('pagination')[0]
 var var7 = document.getElementsByClassName('pagination')[1]
 var var9 = document.getElementsByClassName('pagination')[2]
@@ -27,6 +27,7 @@ $('#right').on('click', () => {
     setTimeout(() => {
         $('.spinner').css('display','none')
     }, 2000)
+
 });
 
 $('#left').on('click', () => {
@@ -59,14 +60,14 @@ function func() {
             var2.value=stringarray[4]
             var5.value=stringarray[5]
         }
-        var6.style.opacity = "0.5";
+        var6.style.background = "rgba(200,200,200,1)";
         setTimeout(() => {
             var1.setAttribute('name', 'company')
             var2.setAttribute('name', 'income')
             var1.setAttribute('placeholder', "Company Name");
             var2.setAttribute('placeholder', "Net Monthly Income");
             var3.style.display = "none"
-            var6.style.opacity = "1";
+            var6.style.background = "rgb(240,240,240)";
             var5.style.display = "block"
             var8.style.display = "block"
         }, 1000)
@@ -76,7 +77,7 @@ function func() {
         stringarray[3]=var1.value
         stringarray[4]=var2.value
         stringarray[5]=var5.value
-        var6.style.opacity = "0.5";
+        var6.style.background = "rgba(200,200,200,1)";
         clearinp();
         setTimeout(() => {
             var1.setAttribute('name', 'email')
@@ -88,9 +89,9 @@ function func() {
             var5.style.display = "none"
             var3.style.display = "block"
             var4.style.animation = "none";
-            var6.style.opacity = "1";
+            var6.style.background = "rgb(240,240,240)";
+            var9.style.display = "none";
         }, 1000)
-        var9.style.display = "none";
         k=1;
         y = 1;
     }
@@ -112,12 +113,12 @@ function func2() {
     if (x == 1 & y == 0) {
         var2.setAttribute('type', 'text')
         var8.style.display = "none"
-        var6.style.opacity = "0.5";
+        var6.style.background = "rgba(200,200,200,1)";
         var1.value=stringarray[0]
         var2.value=stringarray[1]
         var3.value=stringarray[2]
         setTimeout(() => {
-            var6.style.opacity = "1";
+            var6.style.background = "rgb(240,240,240)";
             var5.style.display = "none"
             var3.style.display = "block"
             var1.setAttribute('name', 'first')
@@ -132,9 +133,9 @@ function func2() {
         var1.value=stringarray[3]
         var2.value=stringarray[4]
         var5.value=stringarray[5]
-        var6.style.opacity = "0.5";
+        var6.style.background = "rgba(200,200,200,1)";
         setTimeout(() => {
-            var6.style.opacity = "1";
+            var6.style.background = "rgb(240,240,240)";
             var5.style.display = "block"
             var9.style.display = "block"
             var7.style.display = "none";
